46a7ef7e4836a7b4427ba6ff8e7cc9438443191d
jdm199605 <s3962447@student.rmit.edu.au>
2023-02-28 09:15:34 +0800

    code

--- nosify.py
+++ nosify.py
@@ -1,9 +1,14 @@
 import numpy as np
 import argparse
+from global_variables import PATH
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--data', type = str, default = 'taxi')
+parser.add_argument('--data', type = str, default = 'imdbr')
+parser.add_argument('--r', type = float, default = 5)
+parser.add_argument('--pos', type = int, default = 1)
 data = parser.parse_args().data
+r = parser.parse_args().r
+pos = parser.parse_args().pos
 
 probs = [0.2, 0.4, 0.6, 0.8]
 
@@ -20,7 +25,7 @@ if data in ['imdbc','covtype']:
             if np.random.random() < prob:
                 Y[i] = (train_y[i]+1) % num_classes
 
-        path = f'./data/{data}-train-y-asym-{prob}.npy'
+        path = PATH + f'{data}-train-y-asym-{prob}.npy'
         np.save(path, Y)
 
         Y = train_y.copy()
@@ -31,15 +36,15 @@ if data in ['imdbc','covtype']:
                     new_y = np.random.randint(num_classes)
                 Y[i] = new_y
 
-        path = f'./data/{data}-train-y-sym-{prob}.npy'
+        path = PATH + f'{data}-train-y-sym-{prob}.npy'
         np.save(path, Y)
 else:
     for prob in probs:
         Y = train_y.copy()
         bound = max(Y)
-        noises = np.random.binomial(1, prob, (len(Y),)) * np.random.uniform(0, 5*bound, (len(Y),))
+        noises = np.random.binomial(1, prob, (len(Y),)) * np.random.uniform(0, r*bound, (len(Y),)) if pos else np.random.binomial(1, prob, (len(Y),)) * np.random.uniform(-r*bound, r*bound, (len(Y),))
         Y += noises
         
-        path = f'./data/{data}-train-y-{prob}'
+        path = PATH + f'{data}-train-y-{prob}'
         np.save(path, Y)
         
